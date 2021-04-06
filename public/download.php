629e3596f9886cbbe95ecf9566745f8c4b380e4e
weaponsforge <forgeweapons@gmail.com>
2021-04-07 02:38:50 +0800

    Initial commit.

---
+++ public/download.php
@@ -0,0 +1,43 @@
+<?php
+  // Fetch a JSON response containing a base64-encoded PDF field from 'url'
+  if (isset($_POST['url'])) {
+	$url = $_POST['url'];
+	  
+    // Send POST request to external 'url' server
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($curl);
+    curl_close($curl);
+    
+    // Convert result to json
+    $data = json_decode($result);
+
+    // Echo the fetched base64 string
+    // echo $data->FileContents;
+    
+    // Decode pdf content
+    $decoded = base64_decode($data->FileContents);
+    
+    // Write to pdf file to PHP server disk
+    $file = 'test.pdf';
+    file_put_contents($file, $decoded);
+    
+    // Send the PDF file as response
+    if (file_exists($file)) {
+      header('Content-Description: File Transfer');
+      header('Content-Type: application/octet-stream');
+      header('Content-Disposition: attachment; filename="download.pdf"');
+      header('Expires: 0');
+      header('Cache-Control: must-revalidate');
+      header('Pragma: public');
+      header('Content-Length: ' . filesize($file));
+      readfile($file);
+        
+      // Delete pdf file from PHP server disk
+      unlink($file);
+      exit;
+    }
+  }
+?>
---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta curlarset="utf-8">
+  <meta name="viewport" content="width=device-width; iniital-scale=1.0">
+  <title>Base64 Encode</title>
+</head>
+<body>
+  <h1>PHP Base64 Download</h1>
+ 
+  <!-- FORM -->
+  <form action="download.php" method="POST">
+	<label>URL</label>
+    <input type="text" name="url" value="https://base64pdf.herokuapp.com/getpdf">
+	  <br />
+    <input type="submit" value="Get Base64 convert to PDF" />
+  </form>
+
+</body>
+</html>
